578dd04b35bc5e77a379e991e7655daa26b7fb5e
“CollinABullock” <cbullock@schoolofrock.com>
2023-09-14 23:17:02 -0500

    added links and routes

---
+++ workshop28/src/components/blue.jsx
@@ -0,0 +1,7 @@
+export default function blue() {
+  return (
+    <div className="blue">
+      <h1>I'm blue Da ba dee da ba di</h1>
+    </div>
+  );
+}
---
+++ workshop28/src/components/red.jsx
@@ -0,0 +1,7 @@
+export default function red() {
+  return (
+    <div className="red">
+      <h1>Loving him was red (that's a Taylor Swift song, it's best red based lyric I could find)</h1>
+    </div>
+  );
+}
